c2967a5fc3588ac1787c9b402df7f56594b92604
松本 和馬 <hello@mtmtkzm.com>
2016-11-17 03:54:09 +0900

    各三角形の色をランダムに塗る処理を追加

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,16 +1,21 @@
 
 /* Triangle Animation
 ***** */
+
 // 描画範囲
 var canvas = document.getElementById('js-triangles');
 
 // 三角形DOMを生成
 function createTriangle() {
-  var amount = window.innerWidth * window.innerHeight / 150000; // 面積によって
+  var amount = window.innerWidth * window.innerHeight / 80000; // 面積によって
+  var triangleColors = ['#7B84B3', '#5D71D2', '#4558B3', '#152573'];
+  if(amount > 9) {
+    amount = 9;
+  }
   for (var i=0; i<amount; i++) {
     var triangle = document.createElement('span');
     triangle.classList.add('js-triangle', 'icon', 'icon-triangle');
-    triangle.style.color = '#7B84B3'; // '#5D71D2'; // ベースの青
+    triangle.style.color = triangleColors[ Math.floor( Math.random()*triangleColors.length ) ];
     triangle.style.top = Math.random()*100 + 'vh';
     triangle.style.left = Math.random()*100 + 'vw';
     canvas.appendChild(triangle);
